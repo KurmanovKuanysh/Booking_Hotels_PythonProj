af23981822078844892c92608457db9000ddf351
KurmanovKuanysh <kukatop01@gmail.com>
2026-02-10 16:45:24 +0500

    (feat) Booking , check availability logic,check date range

--- services/booking_service.py
+++ services/booking_service.py
@@ -2,6 +2,11 @@ from models.booking import Booking
 from models.room import Room
 from storage import Storage
 
+
+
+def check_date_range(checkin_date, checkout_date):
+    return checkin_date <= checkout_date
+
 class BookingService:
     def __init__(self, storage: Storage):
         self.storage = storage
@@ -23,3 +28,16 @@ class BookingService:
         )
         self.bookings[new_id] = new_booking
         print("New order, BOOKED!")
+
+
+    def check_availability(self,rooms:dict[int, Room],hotel_id,room_id,checkin_date,checkout_date) -> tuple[bool,str]:
+        if hotel_id not in rooms:
+            return False, "Hotel not found"
+        if room_id not in rooms:
+            return False, "Room not found"
+        for booking in self.bookings.values():
+            if booking.room_id == room_id:
+                if not checkout_date <= booking.checkin_date and not checkin_date >= booking.checkout_date:
+                    return False, "Room is not available on date"
+                return False, "Room is already booked"
+        return True, "Room is available"
