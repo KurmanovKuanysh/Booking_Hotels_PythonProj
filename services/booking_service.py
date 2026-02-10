738462d478774307afb460d668bad6c063f5641d
KurmanovKuanysh <kukatop01@gmail.com>
2026-02-10 18:10:14 +0500

    (fix)In Booking logic, fixed datein/out type, check avail.

--- services/booking_service.py
+++ services/booking_service.py
@@ -1,8 +1,10 @@
 from models.booking import Booking
 from models.room import Room
 from storage import Storage
+from datetime import datetime
 
-
+def parse_date(date_in: str, date_out:str):
+    return datetime.strptime(date_in, "%Y-%m-%d").date(), datetime.strptime(date_out, "%Y-%m-%d").date()
 
 def check_date_range(checkin_date, checkout_date):
     return checkin_date <= checkout_date
@@ -12,7 +14,8 @@ class BookingService:
         self.storage = storage
         self.bookings: dict[int, Booking] = storage.load_bookings()
 
-    def create_new_booking(self, rooms:dict[int, Room],hotel_id,room_id,name_client,checkin_date,checkout_date):
+    def create_new_booking(self, rooms:dict[int, Room],hotel_id,room_id,name_client,checkin_date,checkout_date) -> Booking | None:
+        parse_date(checkin_date,checkout_date)
         room = rooms[room_id]
         new_id = max(self.bookings.keys()) + 1
         price = room.price_for_day * 5
@@ -30,14 +33,11 @@ class BookingService:
         print("New order, BOOKED!")
 
 
-    def check_availability(self,rooms:dict[int, Room],hotel_id,room_id,checkin_date,checkout_date) -> tuple[bool,str]:
-        if hotel_id not in rooms:
-            return False, "Hotel not found"
+    def check_availability(self,rooms:dict[int, Room],room_id,checkin_date,checkout_date) -> tuple[bool,str]:
         if room_id not in rooms:
             return False, "Room not found"
         for booking in self.bookings.values():
             if booking.room_id == room_id:
-                if not checkout_date <= booking.checkin_date and not checkin_date >= booking.checkout_date:
+                if not checkout_date <= booking.checkin_date or checkin_date >= booking.checkout_date:
                     return False, "Room is not available on date"
-                return False, "Room is already booked"
         return True, "Room is available"
