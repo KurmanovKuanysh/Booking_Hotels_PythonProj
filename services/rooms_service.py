94601c5c7bb6d2338f3d9a3a5d4ad5bc62299599
KurmanovKuanysh <kukatop01@gmail.com>
2026-02-10 16:05:55 +0500

    (feat) RoomService Logic, get by id, hotel_id.

--- services/rooms_service.py
+++ services/rooms_service.py
@@ -1,4 +1,17 @@
+from models.room import Room
+from storage import Storage
 
-class Rooms:
-    def __init__(self, rooms):
-        self.rooms = rooms
+class RoomsService:
+    def __init__(self, storage: Storage):
+        self.storage = storage
+        self.rooms: dict[int, Room] = storage.load_rooms()
+
+    def get_by_h_id(self, h_id:int) -> dict[int,Room]:
+        rooms_in_hotel = {}
+        for room in self.rooms.values():
+            if room.hotel_id == h_id:
+                rooms_in_hotel[room.r_id] = room
+        return rooms_in_hotel
+    def get_by_id(self, r_id:int) -> Room | None:
+        return self.rooms.get(r_id)
+
