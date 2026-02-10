6fd8e63aeb875f69c1398b4699761d464271e96e
KurmanovKuanysh <kukatop01@gmail.com>
2026-02-10 15:22:09 +0500

    (feat)Hotel_Service logic, Find by name,get by id.

--- services/hotel_service.py
+++ services/hotel_service.py
@@ -1,1 +1,20 @@
 #check if room exists
+from models.hotel import Hotel
+from storage import Storage
+
+class HotelService:
+    def __init__(self, storage: Storage):
+        self.storage = storage
+        self.hotels: dict[int, Hotel] = storage.load_hotels()
+
+    def find_by_name(self, name: str) -> dict[int,Hotel]:
+        if not name:
+            return {}
+        hotels_found = {}
+        for hotel in self.hotels.values():
+            if name.lower() in hotel.name.lower():
+                hotels_found[hotel.hotel_id] = hotel
+        return hotels_found
+
+    def get_by_id(self, hotel_id: int) -> Hotel | None:
+        return self.hotels.get(hotel_id)
