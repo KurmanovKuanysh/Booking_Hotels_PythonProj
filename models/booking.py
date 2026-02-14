0c5a327f0eaa721b9583090539445e7fe0653c99
KurmanovKuanysh <kukatop01@gmail.com>
2026-02-14 09:23:40 +0500

    (fix)Booking structure

--- models/booking.py
+++ models/booking.py
@@ -1,14 +1,17 @@
 from dataclasses import dataclass
-from datetime import date
+from datetime import date, datetime
 
 @dataclass
 class Booking:
     booking_id:int
     hotel_id:int
     r_id:int
+    guest_name:str
+    guest_email:str
     name_client:str
     checkin_date:date
     checkout_date:date
     total_price:float
     status: str
+    created_at:datetime
 
