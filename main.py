284cf3cf295c439a7d3dbe864f0f996e3fb0acac
KurmanovKuanysh <kukatop01@gmail.com>
2026-02-20 16:41:54 +0500

    (fix) Main Logic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from models.room_types import RoomType
 from services.hotel_service import HotelService
 from storage import Storage
 from services.booking_service import BookingService
@@ -7,6 +8,7 @@ from views.printers import Printers
 from services.input_service import Inputs
 from models.filter import Filter
 from views.menus import Menu
+from services.admin_service import Admin
 from app import App
 
 def main():
@@ -19,16 +21,21 @@ def main():
     filter_service = FilterService(Filter())
     printers = Printers()
     inputs = Inputs()
+    rooms_types = RoomType
+    admin = Admin(storage)
     menu = Menu()
 
     app = App(
+            storage=storage,
             booking_s=bookings_service,
             hotel_s=hotels_service,
             room_s=rooms_service,
             printers=printers,
             input_service=inputs,
             filters=filter_service,
-            menu=menu
+            menu=menu,
+            room_typex = rooms_types,
+            admin = admin
         )
     app.main_menu_flow()
 
