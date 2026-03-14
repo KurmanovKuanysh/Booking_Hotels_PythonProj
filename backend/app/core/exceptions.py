4f7565fd5f953c04dcc59c4b3a3985e43a5fea51
KurmanovKuanysh <kukatop01@gmail.com>
2026-03-15 02:51:28 +0500

    (feat) Exceptions, Errors

---
+++ backend/app/core/exceptions.py
@@ -0,0 +1,20 @@
+class AppError(Exception):
+    pass
+
+class UserNotFoundError(AppError):
+    pass
+
+class DuplicateEmailError(AppError):
+    pass
+
+class UserValidationError(AppError):
+    pass
+
+class InvalidUserRoleError(UserValidationError):
+    pass
+
+class RoomNotAvailableError(AppError):
+    pass
+
+class InvalidRoomTypeError(AppError):
+    pass
