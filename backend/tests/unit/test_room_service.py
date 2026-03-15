0d78597df3dfac0e1080948f718c24fe08c705b6
KurmanovKuanysh <kukatop01@gmail.com>
2026-03-15 16:52:50 +0500

    (fix) Room Test

--- backend/tests/unit/test_room_service.py
+++ backend/tests/unit/test_room_service.py
@@ -1,17 +1,40 @@
 import pytest
+
+from backend.app.models import Hotel, RoomType
 from backend.app.services.room import RoomService
+from backend.app.services.hotel import HotelService
 from backend.app.models.room import Room
 
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 
+from backend.app.services.room_type import RoomTypeService
+
 @pytest.fixture
-def room_one(session: Session):
+def hotel_one(session: Session):
+    service = HotelService(session)
+
+    hotel = service.add_hotel(
+        name="Test Hotel",
+        city="Test City",
+        stars=4.5,
+        address="123 Test Street"
+    )
+    return hotel
+
+@pytest.fixture
+def room_type_one(session: Session):
+    service = RoomTypeService(session)
+
+    return service.add_type("Deluxe")
+
+@pytest.fixture
+def room_one(session: Session, hotel_one, room_type_one):
     service = RoomService(session)
     room = service.add_room(
-        h_id=1,
+        h_id=hotel_one.id,
         room_number="100",
-        r_t_id=1,
+        r_t_id=room_type_one.id,
         capacity=2,
         price_per_day=100,
         floor=1,
@@ -19,12 +42,14 @@ def room_one(session: Session):
     )
     return room
 
-def test_add_room_return_room(session: Session):
+
+
+def test_add_room_return_room(session: Session, hotel_one, room_type_one):
     service = RoomService(session)
     room = service.add_room(
-        h_id=1,
+        h_id=hotel_one.id,
         room_number="100",
-        r_t_id=1,
+        r_t_id=room_type_one.id,
         capacity=2,
         price_per_day=100,
         floor=1,
@@ -42,13 +67,13 @@ def test_add_room_return_none(session: Session, room_one: Room):
     room = service.get_room_by_id(-1)
     assert room is None
 
-def test_add_room_duplicate_room_number(session: Session, room_one: Room):
+def test_add_room_duplicate_room_number(session: Session, room_one: Room, hotel_one, room_type_one):
     service = RoomService(session)
     with pytest.raises(ValueError, match="Room number already exists on Rooms"):
         service.add_room(
-            h_id=1,
+            h_id=hotel_one.id,
             room_number="100",
-            r_t_id=1,
+            r_t_id=room_type_one.id,
             capacity=2,
             price_per_day=100,
             floor=1,
@@ -65,7 +90,7 @@ def test_get_rooms_return_none(session: Session):
     service = RoomService(session)
     room = service.get_rooms()
 
-    assert room is None
+    assert room == []
 
 def test_get_room_by_id_return_room(session: Session, room_one: Room):
     service = RoomService(session)
