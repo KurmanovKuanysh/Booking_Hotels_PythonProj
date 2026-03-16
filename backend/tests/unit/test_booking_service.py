e0faa8601d08ec5c2f19972b93d035819612e1cc
KurmanovKuanysh <kukatop01@gmail.com>
2026-03-16 17:02:42 +0500

    (feat) Booking TEST logic

--- backend/tests/unit/test_booking_service.py
+++ backend/tests/unit/test_booking_service.py
@@ -1,5 +1,89 @@
-# def test_booking_service():
-#     assert 1 == 1
-#
-# def test_2():
-#     assert 2 == 2
+import pytest
+from backend.app.services.booking import BookingService
+from backend.app.services.hotel import HotelService
+from backend.app.services.room import RoomService
+from backend.app.services.room_type import RoomTypeService
+from backend.app.services.user import UserService
+from backend.app.models.booking import Booking
+
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+from datetime import date, timedelta
+
+@pytest.fixture
+def user_one(session: Session):
+    service = UserService(session)
+    user = service.add_user(
+        name="Test User",
+        email="testuser@gmail.com",
+        password="password",
+        role="USER"
+    )
+    return user
+
+@pytest.fixture
+def booking_one(session: Session, room_one, user_one):
+    service = BookingService(session)
+
+    booking = Booking(
+        r_id = room_one.id,
+        check_in = date.today() + timedelta(days=3),
+        check_out= date.today() + timedelta(days=4),
+        status = "confirmed",
+        user_id = user_one.id
+    )
+    session.add(booking)
+    session.commit()
+    session.refresh(booking)
+
+    return booking
+
+@pytest.fixture
+def hotel_one(session: Session):
+    service = HotelService(session)
+
+    hotel = service.add_hotel(
+        name="Test Hotel",
+        city="Test City",
+        stars=4.5,
+        address="123 Test Street"
+    )
+    return hotel
+
+@pytest.fixture
+def room_one(session: Session, hotel_one, room_type_one):
+    service = RoomService(session)
+    room = service.add_room(
+        h_id=hotel_one.id,
+        room_number="100",
+        r_t_id=room_type_one.id,
+        capacity=2,
+        price_per_day=100,
+        floor=1,
+        description="Test Room"
+    )
+    return room
+
+@pytest.fixture
+def room_type_one(session: Session):
+    service = RoomTypeService(session)
+
+    return service.add_type("Deluxe")
+
+def test_add_booking_return_booking(session: Session, room_one, user_one):
+    service = BookingService(session)
+
+    booking = service.create_new_booking(
+        r_id = room_one.id,
+        check_in = date.today() + timedelta(days=3),
+        check_out= date.today() + timedelta(days=4),
+        status = "confirmed",
+        user_id = user_one.id
+    )
+    result = session.scalar(
+        select(Booking)
+        .where(Booking.id == booking.id)
+    )
+
+    assert result == booking
