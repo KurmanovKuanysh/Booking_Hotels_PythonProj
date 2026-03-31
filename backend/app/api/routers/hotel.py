06776b638eccd2e022c2ca4bff88701eda350cb8
KurmanovKuanysh <kukatop01@gmail.com>
2026-03-31 10:24:35 +0500

    (fix) Config logic

--- backend/app/api/routers/hotel.py
+++ backend/app/api/routers/hotel.py
@@ -3,70 +3,36 @@ from sqlalchemy.orm import Session
 from backend.app.services.hotel import HotelService
 from backend.app.api.deps import get_db
 from backend.app.schemas.hotel import HotelRead, HotelBase, HotelEdit
+from backend.app.models.filter import FHotel
 
-router = APIRouter(prefix="/hotels", tags=["Hotels"])
+router = APIRouter(tags=["Hotels"])
 
 
-@router.post("/", response_model=HotelRead)
-def create_hotel(hotel: HotelBase, db: Session = Depends(get_db)):
-    service = HotelService(db)
-    return service.add_hotel(
-        name=hotel.name,
-        city=hotel.city,
-        stars=hotel.stars,
-        address=hotel.address,
-        description=hotel.description
-    )
-
-@router.get("/", response_model=list[HotelRead])
+@router.get("/hotels", response_model=list[HotelRead])
 def get_hotels(db: Session = Depends(get_db)):
     service = HotelService(db)
     return service.get_hotels()
 
-@router.get("/search/address", response_model=list[HotelRead])
+@router.get("/hotels/search/address", response_model=list[HotelRead])
 def get_hotels_by_address(address: str, db: Session = Depends(get_db)):
     service = HotelService(db)
     return service.get_hotels_by_address(address)
 
-@router.get("/search/name", response_model=list[HotelRead])
+@router.get("/hotels/search/name", response_model=list[HotelRead])
 def get_hotels_by_name(name: str, db: Session = Depends(get_db)):
     service = HotelService(db)
     return service.get_hotels_by_name(name)
 
-@router.get("/filter", response_model=list[HotelRead])
+@router.get("/hotels/filter", response_model=list[HotelRead])
 def get_hotels_by_filter(
-        stars_from: float = 1,
-        stars_to: float = 5,
-        city: str | None = None,
+        filters: FHotel = Depends(),
         db: Session = Depends(get_db)
 ):
     service = HotelService(db)
     return service.list_hotels_by_filter(
-        stars_from=stars_from,
-        stars_to=stars_to,
-        city=city
+        filters
     )
-@router.patch("/{hotel_id}/edit", response_model=HotelRead)
-def edit_hotel(
-        hotel_id: int,
-        data: HotelEdit,
-        db: Session = Depends(get_db)
-):
-    service = HotelService(db)
-    return service.edit_hotel(
-        hotel_id=hotel_id,
-        name=data.name,
-        city=data.city,
-        address=data.address,
-        stars=data.stars,
-        description=data.description
-    )
-@router.get("/{hotel_id}", response_model=HotelRead)
+@router.get("/hotels/{hotel_id}", response_model=HotelRead)
 def get_hotel_by_id(hotel_id: int, db: Session = Depends(get_db)):
     service = HotelService(db)
     return service.get_hotel_by_id(hotel_id)
-@router.delete("/{hotel_id}", response_model=bool)
-def delete_hotel(hotel_id: int, db: Session = Depends(get_db)):
-    service = HotelService(db)
-    return service.delete_hotel(hotel_id)
-
--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -10,3 +10,9 @@ DB_HOST = os.getenv('DB_HOST', 'localhost')
 DB_PORT = os.getenv('DB_PORT', '5432')
 
 DB_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+def get_db_url():
+    return DB_URL
+
+def get_auth_data():
+    return {"secret_key": os.getenv('SECRET_KEY'), "algorithm": os.getenv('ALGORITHM')}
