46ed79b6edc8b81ffc60d1f52146a203a6c3c482
KurmanovKuanysh <kukatop01@gmail.com>
2026-04-01 22:42:38 +0500

    (feat) Room , Booking Shemas Upgrade

--- backend/app/schemas/booking.py
+++ backend/app/schemas/booking.py
@@ -14,7 +14,6 @@ class BookingNew(BaseModel):
     check_in: date
     check_out: date
     status: str = Field(min_length=3, max_length=100)
-    total_price: float = Field(gt=0)
 
 class BookingRead(BaseModel):
     id: int = Field(gt=0)
@@ -31,5 +30,8 @@ class BookingEdit(BaseModel):
     r_id: int | None = Field(default=None, gt=0)
     check_in: date | None = Field(default=None)
     check_out: date | None = Field(default=None)
+
+class BookingEditAdmin(BookingEdit):
     status: str | None = Field(default=None, min_length=3, max_length=100)
+    total_price: float | None = Field(default=None, gt=0)
     user_id: int | None = Field(default=None, gt=0)
--- backend/app/schemas/room.py
+++ backend/app/schemas/room.py
@@ -1,4 +1,8 @@
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field, ConfigDict, model_validator
+from datetime import date
+
+from backend.app.core.exceptions import DatesConflictError
+
 
 class RoomCreate(BaseModel):
     room_number: str = Field(min_length=1, max_length=10)
@@ -26,3 +30,25 @@ class RoomEdit(BaseModel):
     price_per_day: float | None =Field(default=None, gt=0)
     floor: int | None = Field(default=None, ge=1)
     description: str | None = Field(default=None,min_length=1,max_length=255)
+
+class RoomAvailable(BaseModel):
+    city: str | None = Field(default=None, min_length=3, max_length=100)
+    check_in: date | None = Field(default=None)
+    check_out: date | None = Field(default=None)
+    guests: int | None = Field(default=None, ge=1)
+
+    @model_validator(mode='after')
+    def check_dates_conflict(self):
+        if self.check_in and self.check_out and self.check_in > self.check_out:
+            raise DatesConflictError
+        return self
+
+class RoomDate(BaseModel):
+    check_in: date
+    check_out: date
+
+    @model_validator(mode='after')
+    def check_dates_conflict(self):
+        if self.check_in and self.check_out and self.check_in > self.check_out:
+            raise DatesConflictError
+        return self
