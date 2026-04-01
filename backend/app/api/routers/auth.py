5a2371facfd508711e832f4ea6d25ab952964cea
KurmanovKuanysh <kukatop01@gmail.com>
2026-04-01 17:51:34 +0500

    (fix) Auth Logic

--- backend/app/api/routers/auth.py
+++ backend/app/api/routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from backend.app.api.deps import get_db, validate_auth_user
 from backend.app.core.exceptions import DuplicateEmailError
 from backend.app.schemas.auth import Token
@@ -11,23 +11,6 @@ from datetime import timedelta
 REFRESH_TOKEN_EXPIRE_DAYS = 2
 router = APIRouter(tags=["Authorization"])
 
-@router.post("/auth/register-admin", response_model=UserRead, status_code=201)
-def create_user_account(
-        user_data: UserRegister,
-        db: Session = Depends(get_db)
-):
-    service = UserService(db)
-
-    if service.get_user_by_email(str(user_data.email)) is not None:
-        raise DuplicateEmailError(email=str(user_data.email))
-
-    new_user = service.register_user(
-        name=user_data.name,
-        email=str(user_data.email),
-        password=user_data.password,
-        role="ADMIN"
-    )
-    return new_user
 @router.post("/auth/register", response_model=UserRead, status_code=201)
 def create_user_account(
         user_data: UserRegister,
@@ -35,9 +18,8 @@ def create_user_account(
 ):
     service = UserService(db)
 
-    if service.get_user_by_email(str(user_data.email)) is not None:
-        raise DuplicateEmailError(email=user_data.email)
-
+    if service.find_user_by_email(str(user_data.email)) is not None:
+        raise DuplicateEmailError #BAD REQUEST
     new_user = service.register_user(
         user_data.name,
         str(user_data.email),
