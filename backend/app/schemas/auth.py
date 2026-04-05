c05f77e22667abc582851a507c85e946391025ba
KurmanovKuanysh <kukatop01@gmail.com>
2026-04-05 12:59:39 +0500

    (feat) Auth Schemas structure

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,9 +1,24 @@
 from pydantic import BaseModel
 
-class Token(BaseModel):
+class TokenPair(BaseModel):
     access_token: str
     refresh_token: str | None = None
-    token_type: str
+    token_type: str = "bearer"
 
 class TokenData(BaseModel):
+    sub: str
     email: str | None = None
+    role: str | None = None
+    type: str = "access"
+
+class RefreshTokenData(BaseModel):
+    sub: str
+    email: str | None = None
+    role: str | None = None
+    type: str = "refresh"
+
+class RefreshRequest(BaseModel):
+    refresh_token: str
+
+class LogOutRequest(BaseModel):
+    refresh_token: str
