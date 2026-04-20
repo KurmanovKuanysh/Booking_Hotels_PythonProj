25c62b1eaea1522a3118e0350fa1676c11a72f71
KurmanovKuanysh <kukatop01@gmail.com>
2026-04-20 15:41:50 +0500

    (fix) Booking Router Fixed bugs

--- backend/app/api/routers/booking.py
+++ backend/app/api/routers/booking.py
@@ -2,26 +2,22 @@ from fastapi import APIRouter, Depends
 from backend.app.api.deps import get_db, get_current_user
 from sqlalchemy.orm import Session
 
-from backend.app.schemas.booking import BookingBase, BookingRead, BookingEdit, BookingNew
+from backend.app.schemas.booking import BookingRead, BookingEdit, BookingCreate, BookingCancelResponse
 from backend.app.schemas.user import UserRead
 from backend.app.services.booking import BookingService
 
 router = APIRouter(tags=["Bookings"])
 
-@router.post("/bookings", response_model=BookingBase)
+@router.post("/bookings", response_model=BookingRead, status_code=201)
 def create_booking(
-    booking: BookingNew,
+    booking: BookingCreate,
     user: UserRead = Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
     service = BookingService(db)
     return service.create_new_booking(
-        r_id=booking.r_id,
-        guest_count=booking.guest_count,
-        check_in=booking.check_in,
-        check_out=booking.check_out,
-        status="pending",
         user_id=user.id,
+        data=booking
     )
 
 @router.get("/bookings/me", response_model=list[BookingRead])
@@ -35,22 +31,17 @@ def get_user_bookings(
 @router.patch("/bookings/{booking_id}", response_model=BookingRead)
 def edit_booking_user(
         booking_id: int,
-        booking: BookingEdit,
+        data: BookingEdit,
         db: Session = Depends(get_db),
         user: UserRead = Depends(get_current_user)
 ):
     service = BookingService(db)
-
-    edit = BookingEdit(
-        check_in=booking.check_in,
-        check_out=booking.check_out,
-    )
     return service.edit_booking_user_side(
-        user=user,
+        user_id=user.id,
         booking_id=booking_id,
-        edit=edit
+        edit=data
     )
-@router.patch("/bookings/{booking_id}/cancel", response_model=BookingRead)
+@router.patch("/bookings/{booking_id}/cancel", response_model=BookingCancelResponse)
 def cancel_booking(
         booking_id: int,
         db: Session = Depends(get_db),
@@ -59,5 +50,5 @@ def cancel_booking(
     service = BookingService(db)
     return service.cancel_booking(
         booking_id=booking_id,
-        user=user,
+        user_id=user.id,
     )
